4f6555690773ed409cc59206c3acee4977ca0c29
Xaraex <kolejanne@gmail.com>
2024-09-23 14:50:39 +0300

    Tehtävä 10: Lisätty titleChangeHandler ja onChange kuuntelija. Tulostetaan käyttäjän syöttämä arvo konsoliin.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    console.log(event.target.value); // Tulostaa syötetyn arvon konsoliin
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
-          <label>Janne Kolehmainen</label>
-          <input type='text' />
+          <label>Title</label>
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2022-01-01' max='2024-12-31' />
+          <input type='date' min='2022-01-01' max='2024-12-31' onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
