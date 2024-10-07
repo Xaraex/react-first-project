62e70db2e0f68e002f4930b1dde96cad6efc625a
Xaraex <kolejanne@gmail.com>
2024-10-07 13:24:38 +0300

    Tehtävä 14: Siirretty expenses-taulukko komponentin ulkopuolelle ja lisätty addExpenseHandler, joka päivittää tilan edellisten kulujen perusteella

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses'; 
 
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date('2022-07-14'),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date('2022-08-12'),
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 300.00,
-      date: new Date('2023-01-01'),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date('2023-01-09'),
-    },
-  ];
+// Expenses-taulukko siirretty funktion ulkopuolelle ja nimetty dummy_expenses
+const dummy_expenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date('2022-07-14'),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date('2022-08-12'),
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 300.00,
+    date: new Date('2023-01-01'),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date('2023-01-09'),
+  },
+];
 
-  // Uusi addExpenseHandler funktio, joka vastaanottaa child-komponentilta tiedon
+const App = () => {
+  // Määritellään state alkuarvolla dummy_expenses
+  const [expenses, setExpenses] = useState(dummy_expenses);
+
+  // Funktio, joka lisää uuden kulutuksen olemassa olevaan tilaan
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense); // Tämä tulostaa siirretyn tiedon konsoliin
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]; // Lisätään uusi kulutus alkuun ja vanhat perään
+    });
   };
 
   return (
     <div>
       {/* Välitetään addExpenseHandler funktio propsina NewExpense-komponentille */}
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} /> 
+      <Expenses items={expenses} />
       <p>Janne Kolehmainen / VAMK / BIT</p>
     </div>
   );
